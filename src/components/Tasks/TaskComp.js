d46bc9af21b133c75507b258d040e6a6f531c76e
Eyal Engel <108540831+Eyal-Engel@users.noreply.github.com>
2023-01-05 11:22:56 +0200

    trololol

--- src/components/Tasks/TaskComp.js
+++ src/components/Tasks/TaskComp.js
@@ -5,6 +5,7 @@ import AudioIcon from "../assets/AudioIcon";
 import CheckIcon from "../assets/CheckIcon";
 import styled from "styled-components";
 import { parseContent } from "./functions";
+import "./taskCompStyle.css"
 
 //obj for save the Length of time it took the user to do the task
 let objTime = {
@@ -95,22 +96,30 @@ export default function TaskComp(props) {
   const isFocused = () => props.index === props.currentIndex;
 
   return (
-    <ContainerWrapper
-      height={props.height}
-      width={props.wrapperWidth ? props.wrapperWidth : 50}
+    <div className="containerWrapper"
+      style={{
+        width: props.wrapperWidth ? props.wrapperWidth : "50%",
+        height: props.height ? props.height : "90px"
+      }}
     >
-      <TaskContainer
-        backgroundColor={decideColor(props.didFinished)}
-        width={props.width ? props.width : 50}
-        height={props.wideCaro ? props.wideCaro : isFocused() ? 100 : 60}
+      <div className="taskContainer"
+        style={{
+          height: props.wideCaro ? props.wideCaro : isFocused() ? "100%" : "60%",
+          backgroundColor: decideColor(props.didFinished),
+        }}
       >
-        <WhiteContainer
-          width={isFocused() && props.wideCaro === undefined ? 90 : 80}
-          height={isFocused() && props.wideCaro === undefined ? 90 : 80}
+        <div className="whiteContainer"
+          style={{
+            width: isFocused() && props.wideCaro === undefined ? "90%" : "80%",
+            height: isFocused() && props.wideCaro === undefined ? "90%" : "80%",
+
+          }}
         >
-          <ImageWrapper
-            maxHeight={isFocused() && props.wideCaro === undefined ? 58 : 100}
-            borderWidth={isFocused() && props.wideCaro === undefined ? 1 : "0"}
+          <div className="imageWrapper"
+            style={{
+              maxHeight: isFocused() && props.wideCaro === undefined ? "58%" : "100%",
+              // borderWidth: isFocused() && props.wideCaro === undefined ? 1 : "0"
+            }}
           >
             <img
               alt={"task image"}
@@ -119,128 +128,25 @@ export default function TaskComp(props) {
                   ? props.imgUrl
                   : "https://globalimpactnetwork.org/wp-content/themes/globalimpact/images/no-image-found-360x250.png"
               }
-              style={styles.imgStyle}
+              className=" imgStyle"
             />
-          </ImageWrapper>
-          <TopBar
-            display={
-              isFocused() && props.wideCaro === undefined ? "flex" : "none"
-            }
+          </div>
+          <div className="topBar"
+            style={{
+              display: isFocused() && props.wideCaro === undefined ? "flex" : "none",
+            }}
+
           >
             <div style={{ width: "20%", height: "auto", marginTop: "auto" }}>
               <AudioIcon audioUrl={props.audioUrl} />
             </div>
             <div style={{ height: "100%", width: "79.5%", padding: 0 }}>
-              <Text fontSize={3.5}>{props.title}</Text>
-              <Text fontSize={2.8}>{parseContent(props.content)}</Text>
+              <div className="textTaskComp" style={{fontSize: "3.5vw"}}>{props.title}</div>
+              <div className="textTaskComp" style={{fontSize: "2.8vw"}}>{parseContent(props.content)}</div>
             </div>
-          </TopBar>
-        </WhiteContainer>
-      </TaskContainer>
-    </ContainerWrapper>
+          </div>
+        </div>
+      </div>
+    </div >
   );
 }
-
-const styles = {
-  containerBox: {
-    width: "50%",
-    height: "250px",
-    backgroundColor: "white",
-    borderRadius: "2%",
-    borderStyle: "solid",
-    borderWidth: 6,
-    display: "flex",
-    flexDirection: "column",
-  },
-
-  aIconBox: {
-    width: "15%",
-  },
-  cIconBox: {
-    width: "22%",
-  },
-  imgStyle: {
-    width: "100%",
-    height: "auto",
-    objectFit: "cover",
-  },
-  leftBox: {
-    flex: 1,
-  },
-  rightBox: {
-    flex: 5,
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifycontent: "center",
-  },
-  textBox: {
-    height: "17%",
-    alignItems: "center",
-    marginBottom: 5,
-  },
-  textStyle: {
-    fontSize: 20,
-    color: "black",
-  },
-};
-
-const ContainerWrapper = styled.div`
-  width: ${(props) => (props.width ? props.width : 50)}%;
-  height: ${(props) => (props.height ? props.height : 90)}px;
-  margin-left: auto;
-  margin-right: auto;
-`;
-
-const TaskContainer = styled.div`
-  height: ${(props) => (props.height ? props.height : 100)}%;
-  background-color: ${(props) =>
-    props.backgroundColor ? props.backgroundColor : "white"};
-  border-radius: 5pt;
-
-  justify-content: center;
-  align-items: center;
-  display: flex;
-  flex-direction: column;
-`;
-const WhiteContainer = styled.div`
-  background-color: white;
-  width: ${(props) => (props.width ? props.width : 80)}%;
-  height: ${(props) => (props.height ? props.height : 80)}%;
-  padding: ${(props) => (props.padding ? props.padding : "6% 6% 2% 6%    ")};
-  border-radius: 5pt;
-  justify-content: space-between;
-`;
-const TopBar = styled.div`
-  display: ${(props) => props.display};
-  width: 100%;
-  max-height: ${(props) => (props.maxHeight ? props.maxHeight : 42)}%;
-  margin: auto;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: row;
-  overflow: hidden;
-`;
-
-const ImageWrapper = styled.div`
-  width: 100%;
-  display: flex;
-  max-height: ${(props) => (props.maxHeight ? props.maxHeight : 52)}%;
-  padding-bottom: 3px;
-`;
-
-const Text = styled.p`
-  font-size: ${(props) => (props.fontSize ? props.fontSize : 2)}vw;
-  text-align: right;
-  font-family: "Arimo", sans-serif;
-  margin: 0;
-  padding: 0;
-  max-height: 40px;
-`;
-
-const ImageBox = styled.div`
-  overflow: hidden;
-  height: 90%;
-  width: 95%;
-  border-radius: ${(props) => (props.borderRadius ? props.borderRadius : 90)}%;
-`;
