9254dfcc2461e598a38309072dae6c7aa9ea033b
Khalid idies <khalididies95@gmail.com>
2024-12-29 15:22:47 +0200

    fix text size

--- src/components/Tasks/TaskComp.js
+++ src/components/Tasks/TaskComp.js
@@ -199,7 +199,7 @@ export default function TaskComp(props) {
               <div
                 className="textTaskComp"
                 style={{
-                  fontSize: "3.5vw",
+                  fontSize: "2.8vw",
                   textAlign: routes_ltr.includes(
                     parseInt(localStorage.getItem("route_id"))
                   )
